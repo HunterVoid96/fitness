527d2c64732ef6c83fdaf6270a8d435625e6c944
HunterVoid96 <benweeks10@gmail.com>
2016-04-28 14:47:01 +0100

    t
    t

--- Get_Fit_Mobile_App/www/index.js
+++ Get_Fit_Mobile_App/www/index.js
@@ -3,23 +3,23 @@ function save() {
     var field1 = document.getElementById('heightType').value;
 	var field2= document.getElementById('Age').value;
 	var field3= document.getElementById('weightType').value;
-	localStorage.setItem('number0', field1);
-	localStorage.setItem('number1', field2);
-	localStorage.setItem('number2', field3);
+	localStorage.setItem('key0', field1);
+	localStorage.setItem('key1', field2);
+	localStorage.setItem('key2', field3);
 }
 function load() {
 
-var storedValue = localStorage.getItem('number0');
+var storedValue = localStorage.getItem('key0');
 if(storedValue) {
 document.getElementById('heightType').value = storedValue;
 
 }
-var storedValue2 = localStorage.getItem('number1');
+var storedValue2 = localStorage.getItem('key1');
 if(storedValue2) {
 document.getElementById('Age').value = storedValue2;
 
 }
-var storedValue3 = localStorage.getItem('number2');
+var storedValue3 = localStorage.getItem('key2');
 if(storedValue3) {
 document.getElementById('weightType').value = storedValue3;
 
