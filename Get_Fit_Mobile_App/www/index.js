e645c209887476db51378f258ea8fe3b9e95d634
HunterVoid96 <benweeks10@gmail.com>
2016-04-28 14:33:07 +0100

    a
    a

--- Get_Fit_Mobile_App/www/index.js
+++ Get_Fit_Mobile_App/www/index.js
@@ -1,111 +1,26 @@
-
-var fitnessLog;
-
-
-    function TimeStamp() {
-    var d = new Date();
-    var n = d.getTime();
-  
+function save() {
+    var field1 = document.getElementById('heightType').value;
+	var field2= document.getElementById('Age').value;
+	var field3= document.getElementById('weightType').value;
+	localStorage.setItem('number0', field1);
+	localStorage.setItem('number1', field2);
+	localStorage.setItem('number2', field3);
 }
+function load() {
 
-function updateLog1() {
+var storedValue = localStorage.getItem('number0');
+if(storedValue) {
+document.getElementById('heightType').value = storedValue;
 
-	var heightType = $("#heightType").val ();
-	var age = $("#Age").val();
-	var weightType = $("#weightType").val();
-$( "#heightType" ).click(updateLog() {
-  alert( "heightType" );
-   alert( "weightType" );
-    alert( "Age" );
-});
-
-window.localStorage.setItem(
-“heightType”,
-“”
-);
-window.localStorage.setItem(
-“weightType”,
-“”
-);
-window.localStorage.setItem(
-“Age”,
-“”
-);
-	var entry = { "heightType":"++" ,
-	"weightTypeType":"++" ,
-	"Age":"++" 
-	
-	};
-
-
-	fitnessLog.push( { TimeStamp: entry } );
 }
+var storedValue2 = localStorage.getItem('number1');
+if(storedValue2) {
+document.getElementById('Age').value = storedValue2;
 
-function updateLog2() {
-
-	var stepsType = $("#stepsType").val ();
-	var DistanceType = $("#DistanceType").val();
-	var ActiveType = $("ActiveType").val();
-	var CaloriesType = $("CaloriesType").val();
-$( "#stepsType" ).click(updateLog2() 
-$( "#DistanceType" ).click(updateLog2()
-$( "#ActiveType" ).click(updateLog2()
-$( "#CaloriesType" ).click(updateLog2(){
-  alert( "stepsType" );
-   alert( "DistanceType" );
-    alert( "ActiveType" );
-		alert( "CaloriesTypeType" );
-});
-
-window.localStorage.setItem(
-“stepsType”,
-“”
-);
-window.localStorage.setItem(
-“DistanceType”,
-“”
-);
-window.localStorage.setItem(
-“ActiveType”,
-“”
-);
-window.localStorage.setItem(
-“CaloriesType”,
-“”
-);
-	var entry = { "stepsType":"++" ,
-	"DistanceType":"++" ,
-	"ActiveType":"++",
-	"CaloriesType":"++"
-	
-	};
-
-
-	fitnessLog.push( { TimeStamp: entry } );
 }
+var storedValue3 = localStorage.getItem('number2');
+if(storedValue3) {
+document.getElementById('weightType').value = storedValue3;
 
-function addItems() {
-            //Declares the data (height, weight and age).
-            var data1= document.getElementById('heightType');
-            var data2 = document.getElementById('weightType');
-			var data3 = document.getElementById('Age');
-            //Sets the items in local storage.
-            if ("localStorage" in window) {
-                localStorage.setItem(data1.value, data2.value, data3.value);
-                location.reload();
-            } else {
-                alert("no localStorage in window");
-            }
-            //Sets the items in session storage.
-            if ("sessionStorage" in window) {
-                sessionStorage.setItem(key.value, data.value);
-                location.reload();
-            } else {
-                alert("no sessionStorage in window");
-            }
-        }
-
-
-
-
-
+}
+}
