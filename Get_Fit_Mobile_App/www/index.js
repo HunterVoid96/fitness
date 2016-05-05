12a013ccdd697eec6adea6a6c505f09fce2bab61
HunterVoid96 <benweeks10@gmail.com>
2016-05-05 11:11:18 +0100

    a
    a

--- Get_Fit_Mobile_App/www/index.js
+++ Get_Fit_Mobile_App/www/index.js
@@ -8,6 +8,8 @@ function save() {
 	localStorage.setItem('key0', field1);
 	localStorage.setItem('key1', field2);
 	localStorage.setItem('key2', field3);
+		document.getElementById('StepsType').addEventListener('click', function() {
+            navigator.vibrate(1000);
 }
 /* declare load function */
 function load() {
@@ -25,6 +27,8 @@ document.getElementById('Age').value = storedValue2;
 var storedValue3 = localStorage.getItem('key2');
 if(storedValue3) {
 document.getElementById('weightType').value = storedValue3;
+	document.getElementById('StepsType').addEventListener('click', function() {
+            navigator.vibrate(1000);
 
 }
 }
@@ -93,6 +97,8 @@ function delz (){
 	localStorage.removeItem('key6');
 	//clears the data fields
 	location.reload();
+		
+            navigator.vibrate(1000);
 }
 //timer-stopwatch inspired from: https://www.youtube.com/watch?v=jRhB1IG7uAw
 //declare seconds, time and divide variables for stopwatch
@@ -121,4 +127,15 @@ function reset2()
 	stop();
 	i=0;
 	document.getElementById("timer_position").innerHTML = (i/divide);
+}
+
+function onDeviceReady() {
+	navigator.vibrate();
+    console.log(navigator.vibrate);
+	navigator.vibrate(1000);
+	document.getElementById('#input').addEventListener('click', function() {
+            navigator.vibrate(1000);
+			document.getElementById('Me').addEventListener('click', function() {
+            navigator.vibrate(1000);
+	
 }
